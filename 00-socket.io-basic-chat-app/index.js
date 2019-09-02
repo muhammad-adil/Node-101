7c558c466873ff672319a243a5a27a6ad69009b3
Muhammad-Adil <adil.muhammad93@gmail.com>
2019-09-03 01:52:47 +0200

    Rearrange repo

---
+++ 00-socket.io-basic-chat-app/index.js
@@ -0,0 +1,26 @@
+
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(4000, function(){
+	console.log('listening to requests on port 4000')
+});
+
+app.use(express.static('public'));
+
+var io = socket(server);
+
+ io.on('connection', function(socket){
+ 	console.log('made socket connection', socket.id)
+ 	
+ 	 	socket.on('chat', function(data){
+ 	 		io.sockets.emit('chat', data);
+ 	 	});
+
+		socket.on('typing', function(data){
+ 	 		socket.broadcast.eit('typing', data)
+ 	 		// io.sockets.emit('chat', data);
+ 	 	});
+
+ })
---
+++ 00-socket.io-basic-chat-app/readme.md
@@ -0,0 +1,7 @@
+# Socket.io & Node Basic Chat App
+
+This app is using node and socket.io.
+
+- Clone the repo,
+- cd into folder.
+- To start project write node index.js.
