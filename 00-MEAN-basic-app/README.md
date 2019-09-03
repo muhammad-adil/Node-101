5abaf19b9a24899ef4b95096758f4615363bd144
Muhammad-Adil <adil.muhammad93@gmail.com>
2019-09-03 02:04:08 +0200

    Organizing repo

---
+++ 00-MEAN-basic-app/README.md
@@ -0,0 +1,6 @@
+# MeanApp
+Node,Express,Angular
+getting data from fake json server.
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.1.
+
---
+++ 00-MEAN-basic-app/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+import { PostsService } from './posts.service';
+
+// Define the routes
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES) // Add routes to the app
+  ],
+  providers: [PostsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
