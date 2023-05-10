0135c58e666a2d756df683170d7b9335d38e2d3b
muhammad-adil <adil.muhammad93@gmail.com>
2023-05-10 11:22:33 +0200

    02-Node-101 - OS Module

--- 02-Node-101/03-modules.js
+++ 02-Node-101/03-modules.js
@@ -9,8 +9,8 @@
 const names = require('./04-names')
 const sayHi = require('./05-utils')
 const data = require('./06-alternative-flavor')
-
-console.log(data);
+require('./07-mind-grenade')
+// console.log(data);
 
 sayHi('susan')
 sayHi(names.john)
