8cb3f0c54c2cdb68bef5975897e8b2c39073cc39
muhammad-adil <adil.muhammad93@gmail.com>
2023-05-10 11:22:33 +0200

    02-Node-101 - Files and Module

---
+++ 02-Node-101/03-modules.js
@@ -0,0 +1,17 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+// using module of names
+// const john = 'john'
+// const peter = 'peter'
+// const secret = 'SUPER SECRET'
+
+const names = require('./04-names')
+const sayHi = require('./05-utils')
+const data = require('./06-alternative-flavor')
+
+console.log(data);
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
